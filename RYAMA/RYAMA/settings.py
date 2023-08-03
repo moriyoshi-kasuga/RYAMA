91f3570ae62819ec6be5889830648fd856914eb8
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-08-04 00:47:24 +0900

    fix conflict

--- RYAMA/RYAMA/settings.py
+++ RYAMA/RYAMA/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "django_browser_reload",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "RYAMA.urls"
--- RYAMA/base/views.py
+++ RYAMA/base/views.py
@@ -1,18 +1,6 @@
-import json
-
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
-
-# def page_test(request, *args, **kwargs):
-#     return render(request, "test.html")
-
-
-# Create your views here.
-
-
-def page_markdowns(request, *args, **kwargs):
-    return render(request, "markdowns.html", context={"theme": "dark"})
 
 
 def page_home(request, *args, **kwargs):
@@ -21,16 +9,22 @@ def page_home(request, *args, **kwargs):
     return render(request, "homes/home.html")
 
 
-def page_why(request, *args, **kwargs):
-    if request.method == "GET":
-        return render(request, "homes/why.html")
-    return render(request, "homes/why.html")
+def page_publish(request, *args, **kwargs):
+    if request.method == "GEt":
+        return render(request, "homes/publish.html")
+    return render(request, "homes/publish.html")
 
 
-def page_feature(request, *args, **kwargs):
+def page_about(request, *args, **kwargs):
     if request.method == "GET":
-        return render(request, "homes/feature.html")
-    return render(request, "homes/feature.html")
+        return render(request, "homes/about.html")
+    return render(request, "homes/about.html")
+
+
+def page_features(request, *args, **kwargs):
+    if request.method == "GET":
+        return render(request, "homes/features.html")
+    return render(request, "homes/features.html")
 
 
 def page_login(request, *args, **kwargs):
@@ -39,27 +33,3 @@ def page_login(request, *args, **kwargs):
 
 def page_signup(request, *args, **kwargs):
     return render(request, "homes/signup.html")
-
-
-def page_account(request, *args, **kwargs):
-    context = {
-        "usernames": json.dumps(list(User.objects.values_list("username", flat=True)))
-    }
-    if request.method == "GET":
-        return render(request, "homes/account.html", context=context)
-    sign = request.POST["sign"]
-    if sign == "in":
-        username = request.POST["name"]
-        password = request.POST["password"]
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("home")
-        context["in_username"] = username
-        context["in_error"] = "このユーザーは存在しません"
-    elif sign == "up":
-        pass
-    else:
-        pass
-
-    return render(request, "homes/account.html", context=context)
