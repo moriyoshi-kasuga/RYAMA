5b269a201d2f7916447d561c080a57b1c209ddf4
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-08-08 04:18:57 +0900

    add others

--- RYAMA/base/urls.py
+++ RYAMA/base/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     # path("test/", views.page_test, name="test"),
     # NOTE: ↑ of TEST page
     path("", views.page_home, name="home"),
-    path("login", views.page_login, name="login"),
-    path("signup", views.page_signup, name="signup"),
-    path("publish", views.page_publish, name="publish"),
+    path("login/", views.page_login, name="login"),
+    path("signup/", views.page_signup, name="signup"),
+    path("publish/", views.page_publish, name="publish"),
     path("about/", views.page_about, name="about"),
     path("features/", views.page_features, name="features"),
     path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
+    path("markdowns/", views.page_markdowns, name="markdowns"),
 ]
