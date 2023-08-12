94c0a9ec5e129178f0188f6fa68963846a179fa5
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-08-12 22:13:21 +0900

    change eslint extend from standard to airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ['standard'],
-  overrides: [],
-  parserOptions: {},
-  rules: {}
-}
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+  },
+};
--- RYAMA/base/static/js/markdowns/markdowns.js
+++ RYAMA/base/static/js/markdowns/markdowns.js
@@ -1,148 +1,148 @@
-function applyClassElements (className, elementLambda) {
-  const $clazz = document.getElementsByClassName(className)
-  for (let i = 0; i < $clazz.length; i++) {
-    elementLambda($clazz[i])
+function applyClassElements(className, elementLambda) {
+  const $clazz = document.getElementsByClassName(className);
+  for (let i = 0; i < $clazz.length; i += 1) {
+    elementLambda($clazz[i]);
   }
 }
 
 // BLOCK: ContextMenus
-let enableContextMenu = null
-const $contextMenus = document.querySelector('.ContextMenus')
+let enableContextMenu = null;
+const $contextMenus = document.querySelector('.ContextMenus');
 
 const resetContextMenu = () => {
   if (enableContextMenu !== null) {
-    enableContextMenu.style = 'none'
+    enableContextMenu.style = 'none';
   }
-}
+};
 
-const $fileContextMenu = $contextMenus.querySelector('.FileContextMenu')
+const $fileContextMenu = $contextMenus.querySelector('.FileContextMenu');
 
-const $folderContextMenu = $contextMenus.querySelector('.FolderContextMenu')
+const $folderContextMenu = $contextMenus.querySelector('.FolderContextMenu');
 
-function showContextMenu (contextMenu, x, y) {
-  resetContextMenu()
-  enableContextMenu = contextMenu
-  contextMenu.style.display = 'block'
-  contextMenu.style.left = `${x}px`
-  contextMenu.style.top = `${y}px`
+function showContextMenu(contextMenu, x, y) {
+  resetContextMenu();
+  enableContextMenu = contextMenu;
+  contextMenu.style.display = 'block';
+  contextMenu.style.left = `${x}px`;
+  contextMenu.style.top = `${y}px`;
 }
 
 document.addEventListener('click', () => {
-  resetContextMenu()
-})
+  resetContextMenu();
+});
 
 applyClassElements('PaneBody', ($Pane) => {
   $Pane.addEventListener('contextmenu', (event) => {
     if (event.target === event.currentTarget) {
-      console.log(`${event.target.id}ContextMenu`)
+      console.log(`${event.target.id}ContextMenu`);
       showContextMenu(
         $contextMenus.querySelector(`.${event.target.id}ContextMenu`),
         event.pageX,
-        event.pageY
-      )
+        event.pageY,
+      );
     }
-  })
-})
+  });
+});
 
 // BLOCK: ModalMenus
-const $modalMenus = document.querySelector('.ModalMenus')
+const $modalMenus = document.querySelector('.ModalMenus');
 
 applyClassElements('ModalMenu', ($modalMenu) => {
   $modalMenu.addEventListener('click', (event) => {
     if (event.target === event.currentTarget) {
-      $modalMenu.style.display = 'none'
+      $modalMenu.style.display = 'none';
     }
-  })
-  const $ModalMenuClose = $modalMenu.querySelector('.ModalMenu-close')
+  });
+  const $ModalMenuClose = $modalMenu.querySelector('.ModalMenu-close');
   if ($ModalMenuClose !== null) {
     $ModalMenuClose.addEventListener('click', () => {
-      $modalMenu.style.display = 'none'
-    })
+      $modalMenu.style.display = 'none';
+    });
   }
-})
+});
 
-const $FileRenameModalMenu = $modalMenus.querySelector('.FileRenameModalMenu')
+const $FileRenameModalMenu = $modalMenus.querySelector('.FileRenameModalMenu');
 
-function showModalMenuOfFileRename (fileID, name, x, y) {
-  $FileRenameModalMenu.style.display = 'block'
-  $FileRenameModalMenu.style.left = `${x}px`
-  $FileRenameModalMenu.style.top = `${y - 50}px`
-  const $name = $FileRenameModalMenu.querySelector('#FileRenameModalMenu-name')
-  $name.value = name
-  $name.focus()
-  $FileRenameModalMenu.querySelector('#fileRenameModalMenu-id').value = fileID
+function showModalMenuOfFileRename(fileID, name, x, y) {
+  $FileRenameModalMenu.style.display = 'block';
+  $FileRenameModalMenu.style.left = `${x}px`;
+  $FileRenameModalMenu.style.top = `${y - 50}px`;
+  const $name = $FileRenameModalMenu.querySelector('#FileRenameModalMenu-name');
+  $name.value = name;
+  $name.focus();
+  $FileRenameModalMenu.querySelector('#fileRenameModalMenu-id').value = fileID;
 }
 
 // BLOCK: Explorer Pane
-const $LeftPane = document.querySelector('.LeftPaneContainer')
-const $explorerPane = document.getElementById('explorer')
+const $LeftPane = document.querySelector('.LeftPaneContainer');
+const $explorerPane = document.getElementById('explorer');
 
 applyClassElements('explorer-toggle', ($toggle) => {
   $toggle.addEventListener('click', () => {
     if ($LeftPane.style.display === 'block') {
-      $LeftPane.style.display = 'none'
+      $LeftPane.style.display = 'none';
     } else {
-      $LeftPane.style.display = 'block'
+      $LeftPane.style.display = 'block';
     }
-  })
-})
+  });
+});
 
 applyClassElements('explorer-close', ($close) => {
   $close.addEventListener('click', () => {
-    $LeftPane.style.display = 'none'
-  })
-})
+    $LeftPane.style.display = 'none';
+  });
+});
 
 applyClassElements('explorer-open', ($open) => {
   $open.addEventListener('click', () => {
-    $LeftPane.style.display = 'block'
-  })
-})
+    $LeftPane.style.display = 'block';
+  });
+});
 
 applyClassElements('FolderItem', ($folderItem) => {
-  const $folderItemHeader = $folderItem.querySelector('.FolderItem-header')
-  const $folderChildren = $folderItem.querySelector('.pane-item-children')
+  const $folderItemHeader = $folderItem.querySelector('.FolderItem-header');
+  const $folderChildren = $folderItem.querySelector('.pane-item-children');
   $folderItemHeader.addEventListener('click', () => {
     if ($folderItem.classList.contains('empty-item')) {
-      return
+      return;
     }
     if ($folderItem.classList.contains('folder-open')) {
-      $folderItem.classList.remove('folder-open')
-      $folderChildren.style.display = 'none'
+      $folderItem.classList.remove('folder-open');
+      $folderChildren.style.display = 'none';
     } else {
-      $folderItem.classList.add('folder-open')
-      $folderChildren.style.display = 'block'
+      $folderItem.classList.add('folder-open');
+      $folderChildren.style.display = 'block';
     }
-  })
+  });
   $folderItemHeader.addEventListener('contextmenu', (event) => {
-    showContextMenu($folderContextMenu, event.pageX, event.pageY)
-  })
-})
+    showContextMenu($folderContextMenu, event.pageX, event.pageY);
+  });
+});
 
 applyClassElements('FileItem', ($fileItem) => {
-  const $fileItemHeader = $fileItem.querySelector('.FileItem-header')
+  const $fileItemHeader = $fileItem.querySelector('.FileItem-header');
   // NOTE: ここでファイルを開く
   $fileItemHeader.addEventListener('click', () => {
     if ($fileItem.classList.contains('file-open')) {
-      const rect = $fileItem.getBoundingClientRect()
+      const rect = $fileItem.getBoundingClientRect();
       showModalMenuOfFileRename(
         $fileItem.id.replace('File-', ''),
         $fileItemHeader.querySelector('.FileItem-title').textContent,
         rect.x,
-        rect.y
-      )
+        rect.y,
+      );
       // NOTE: edit name
-      return
+      return;
     }
-    const $opendFile = $explorerPane.getElementsByClassName('file-open')
+    const $opendFile = $explorerPane.getElementsByClassName('file-open');
     if ($opendFile.length !== 0) {
-      const $openFile = $opendFile[0]
-      $openFile.classList.remove('file-open')
+      const $openFile = $opendFile[0];
+      $openFile.classList.remove('file-open');
     }
-    $fileItem.classList.add('file-open')
+    $fileItem.classList.add('file-open');
     // NOTE: redirect file preview and editor
-  })
+  });
   $fileItemHeader.addEventListener('contextmenu', (event) => {
-    showContextMenu($fileContextMenu, event.pageX, event.pageY)
-  })
-})
+    showContextMenu($fileContextMenu, event.pageX, event.pageY);
+  });
+});
