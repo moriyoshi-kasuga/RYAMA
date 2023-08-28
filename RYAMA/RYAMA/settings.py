a1c41ee68420e912b2e38f1dcd3bb4f75dd7052f
moriyoshi-kasug <moriyoshi.kasuga1218@gmail.com>
2023-08-28 12:08:58 +0900

    allow access mac

--- RYAMA/RYAMA/settings.py
+++ RYAMA/RYAMA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-50=xie3he0=$uz7jp1wtwn))*vttme&uw(gtbbd#6mniwoky9p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.55", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.1.55", "127.0.0.1", "192.168.1.4"]
 
 
 # Application definition
